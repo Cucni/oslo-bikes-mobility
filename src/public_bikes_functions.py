a69f408580b2bf47985f52f62dc18b288b734c8c
Cucni <22103305+Cucni@users.noreply.github.com>
2022-01-02 19:18:18 +0100

    Refactor functions to load public bikes data
    I have separated data retrieval from data fetching from the server, and
    added a conditional to avoid fetching remote data when not necessary.

--- src/public_bikes_functions.py
+++ src/public_bikes_functions.py
@@ -3,13 +3,26 @@
 #Collection of functions to work with the public bikes data
 
 import pandas as pd
+from os.path import exists
 
-#This function fetches the data from the Oslo Bysykkel server, and then aggregates it in a dataset. It does so for the months specified in the "months" list, and for the year specified in the "year" variable
-def load_years(year,months):
+DATA_FOLDER_RAW = 'data/raw/'
+
+#This function retrieves public bikes data for a single month. If necessary, it fetches data from the Oslo Bysykkel server and stores it locally.
+def load_monthly_data(year,month):
+    if not exists(DATA_FOLDER_RAW + '{:}-{:02d}.csv'.format(year,month)):
+        df = pd.read_csv('https://data.urbansharing.com/oslobysykkel.no/trips/v1/{:}/{:02d}.csv'.format(year,month),parse_dates=[0,1])
+        df.to_csv(DATA_FOLDER_RAW + '{:}-{:02d}.csv'.format(year,month),index=False)
+    else:
+        df = pd.read_csv(DATA_FOLDER_RAW + '{:}-{:02d}.csv'.format(year,month),parse_dates=[0,1])
+
+    return df
+
+#This function loads public bikes data for every month the "months" list, and for the specified year. It returns an aggregated dataset.
+def load_months(year,months):
     datasets = {}
 
     for month in months:
-        datasets[month] = pd.read_csv('https://data.urbansharing.com/oslobysykkel.no/trips/v1/'+year+'/'+month+'.csv',parse_dates=[0,1])
+        datasets[month] = load_monthly_data(year,month)
 
     df = pd.concat(datasets)
     return df
