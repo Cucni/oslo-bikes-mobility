d5b7e783a1641f506b9b22f24e99864027154cdf
Cucni <22103305+Cucni@users.noreply.github.com>
2022-02-04 15:17:32 +0100

    Fix error due to new interface for loading func

--- src/mobility_report_functions.py
+++ src/mobility_report_functions.py
@@ -16,7 +16,7 @@ def load_google(year):
     return df
 
 def plot_google():
-    df = load_google()
+    df = load_google(2020)
     sns.lineplot(data=df[df["date"].dt.month.isin([5,6,7,8])],x="doy",y="transit_stations_percent_change_from_baseline")
     plt.title("Google's relative variation in transit stations mobility")
     plt.ylabel("Transit stations percent variation from baseline")
